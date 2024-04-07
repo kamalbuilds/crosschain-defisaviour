ee57df7b9df847155bb9625512c17609aa11dedb
Kamal Nayan <95926324+kamalbuilds@users.noreply.github.com>
2024-04-07 21:42:18 +0530

    asset imagges

---
+++ frontend/config/site.ts
@@ -0,0 +1,16 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "OP Liquidation Protector",
+  description:
+    "OP Liquidation Protector.",
+  mainNav: [
+    {
+      title: "Dashboard",
+      href: "/dashboard",
+    }
+  ],
+  links: {
+    twitter: "https://twitter.com/0xkamal7",
+  },
+}
