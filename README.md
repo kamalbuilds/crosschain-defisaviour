c76c67483525e9b2b359c03478816a90c2385b5e
Kamal Nayan <95926324+kamalbuilds@users.noreply.github.com>
2024-04-12 15:42:18 +0530

    aave management

---
+++ README.md
@@ -0,0 +1,38 @@
+# OP Liquidition Protector
+
+**Seamlessly Manage Your Liquidity Positions Across Multiple Protocols and Chains powered by Social Login**
+
+## Tagline
+Unlock the full potential of your assets with Liquidity Position Manager - Your gateway to efficient and optimized crosschain liquidity management in the DeFi space.
+
+## Contracts 
+
+
+## Features
+
+- **Social Login with Particle Auth**: Simplify your access with secure and convenient social logins powered by Particle Auth, enabling you to start managing your assets with just a few clicks.
+
+
+- **Covalent API powered User Asset Management**: Gain insights into your investments with real-time analytics and performance metrics.
+
+- **Multi-DEX Support**: Effortlessly manage your liquidity across various decentralized exchanges.
+
+  
+- **User-Friendly Interface**: Navigate through the complexities of DeFi with an intuitive and easy-to-use interface.
+
+- **Secure and Trustworthy**: Built on robust smart contracts to ensure the security and integrity of your investments.
+
+
+- **Real-Time Analytics**: Gain insights into your investments with real-time analytics and performance metrics.
+
+
+## Future Works
+- **Optimized Asset Allocation**: Utilize advanced algorithms to optimize your asset allocation for maximum returns.
+## The Problem
+In the rapidly evolving DeFi landscape, managing liquidity across multiple decentralized exchanges (DEXes) can be cumbersome and inefficient. Traditional methods often lead to suboptimal asset allocation, missed opportunities, and increased exposure to risks.
+
+## The Solution
+Liquidity Manager addresses these challenges by offering a comprehensive platform that simplifies the management of your liquidity. By leveraging cutting-edge technology, smart contract automation, and the ease of social logins via Particle Auth, Liquidity Manager ensures your assets are always positioned to capture the best yields, while minimizing risks and maximizing efficiency.
+
+## Getting Started
+Dive into the world of optimized liquidity management by exploring our [documentation]() and getting hands-on with Liquidity Manager today. Experience the convenience of social logins with Particle Auth and streamline your DeFi operations.
---
+++ frontend/app/aave/page.tsx
@@ -0,0 +1,262 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import dynamic from "next/dynamic"
+import Image from "next/image"
+import {
+  ChainId,
+  InterestRate,
+  Pool,
+  PoolBundle,
+  UiIncentiveDataProvider,
+  UiPoolDataProvider,
+} from "@aave/contract-helpers"
+import {
+  formatReserves,
+  formatUserSummaryAndIncentives,
+} from "@aave/math-utils"
+import { AaveV3Fuji, AaveV3Sepolia } from "@bgd-labs/aave-address-book"
+import * as markets from "@bgd-labs/aave-address-book"
+import {
+  AAWrapProvider,
+  SendTransactionMode,
+  SmartAccount,
+} from "@particle-network/aa"
+import { Avalanche , OptimismSepolia} from "@particle-network/chains"
+import { useAccountInfo } from "@particle-network/connect-react-ui"
+import dayjs from "dayjs"
+import { ethers } from "ethers"
+import { parseUnits } from "ethers/lib/utils"
+import { Activity, CreditCard, DollarSign, Users } from "lucide-react"
+import { toast } from "react-toastify"
+
+import { buttonVariants } from "@/components/ui/button"
+import BarChart from "@/components/BarChart"
+import Card, { CardContent, CardProps } from "@/components/Card"
+import DebtPositionsCard, {
+  DebtPositionsProps,
+} from "@/components/DebtPostionsCard"
+import PageTitle from "@/components/PageTitle"
+import BorrowCard from "@/components/payments/BorrowCard"
+import RepayCard from "@/components/payments/RepayCard"
+import SupplyCard from "@/components/payments/SupplyCard"
+
+export default function Home() {
+  const { particleProvider, account } = useAccountInfo()
+  const [userDetails, setUserDetails] = useState<any>()
+  const [aaveprotocol, setAaveprotocol] = useState<any>(AaveV3Fuji)
+  const [customProvider, setCustomProvider] = useState<any>()
+  const [address, setAddress] = useState<string>()
+  const [balance, setBalance] = useState<string>()
+  const [smartAccount, setSmartAccount] = useState<any>()
+
+  useEffect(() => {
+    if (particleProvider && account) {
+      // @ts-ignore
+      const smartAccount = new SmartAccount(particleProvider, {
+        projectId: process.env.NEXT_PUBLIC_PROJECT_ID || "",
+        clientKey: process.env.NEXT_PUBLIC_CLIENT_KEY || "",
+        appId: process.env.NEXT_PUBLIC_APP_ID || "",
+        aaOptions: {
+          accountContracts: {
+            // 'BICONOMY', 'CYBERCONNECT', 'SIMPLE'
+            BICONOMY: [
+              {
+                version: "1.0.0",
+                chainIds: [OptimismSepolia.id],
+              },
+              {
+                version: "2.0.0",
+                chainIds: OptimismSepolia.id,
+              },
+            ],
+            CYBERCONNECT: [
+              {
+                version: "1.0.0",
+                chainIds: OptimismSepolia.id,
+              },
+            ],
+            SIMPLE: [
+              {
+                version: "1.0.0",
+                chainIds: OptimismSepolia.id,
+              },
+            ],
+          },
+          paymasterApiKeys: [
+            {
+              // Optional
+              chainId: 1,
+              apiKey: "Biconomy Paymaster API Key",
+            },
+          ],
+        },
+      })
+      setSmartAccount(smartAccount)
+      const wrappedProvider = new ethers.providers.Web3Provider(
+        new AAWrapProvider(smartAccount, SendTransactionMode.Gasless),
+        "any"
+      )
+      setCustomProvider(wrappedProvider)
+
+      const fetchBalance = async () => {
+        const address = await smartAccount.getAddress();
+        setAddress(address);
+        if(customProvider) {
+          const balanceResponse = await customProvider.getBalance(address);
+          setBalance(ethers.utils.formatEther(balanceResponse));
+        }
+      }
+
+      fetchBalance()
+    }
+  }, [particleProvider, account])
+
+  const SideNavbarNoSSR = dynamic(() => import("../../components/SideNavbar"), {
+    ssr: false,
+  })
+
+  useEffect(() => {
+    if (particleProvider && account) {
+      const provider = new ethers.providers.Web3Provider(
+              // @ts-ignore
+        particleProvider,
+        "any"
+      )
+
+      ;(async () => {
+        try {
+          const chainName = await provider.getNetwork()
+          const chainid = chainName.chainId
+          if (chainid == 11155111) {
+            setAaveprotocol(AaveV3Sepolia)
+          }
+
+          const poolDataProviderContract = new UiPoolDataProvider({
+            uiPoolDataProviderAddress: aaveprotocol.UI_POOL_DATA_PROVIDER,
+            provider: provider,
+            chainId: chainid,
+          })
+
+          const incentiveDataProviderContract = new UiIncentiveDataProvider({
+            uiIncentiveDataProviderAddress:
+              aaveprotocol.UI_INCENTIVE_DATA_PROVIDER,
+            provider: provider,
+            chainId: chainid,
+          })
+
+          // Fetch data using the contracts
+          const reserves = await poolDataProviderContract.getReservesHumanized({
+            lendingPoolAddressProvider: aaveprotocol.POOL_ADDRESSES_PROVIDER,
+          })
+
+          const userReserves =
+            await poolDataProviderContract.getUserReservesHumanized({
+              lendingPoolAddressProvider: aaveprotocol.POOL_ADDRESSES_PROVIDER,
+              user: account,
+            })
+
+          // Process and display data as needed...
+          // Array of incentive tokens with price feed and emission APR
+          const reserveIncentives =
+            await incentiveDataProviderContract.getReservesIncentivesDataHumanized(
+              {
+                lendingPoolAddressProvider:
+                  aaveprotocol.POOL_ADDRESSES_PROVIDER,
+              }
+            )
+
+          // Dictionary of claimable user incentives
+          const userIncentives =
+            await incentiveDataProviderContract.getUserReservesIncentivesDataHumanized(
+              {
+                lendingPoolAddressProvider:
+                  aaveprotocol.POOL_ADDRESSES_PROVIDER,
+                user: account,
+              }
+            )
+
+          console.log({
+            reserves,
+            userReserves,
+            reserveIncentives,
+            userIncentives,
+          })
+
+          const reservesArray = reserves.reservesData
+          const baseCurrencyData = reserves.baseCurrencyData
+          const userReservesArray = userReserves.userReserves
+
+          const currentTimestamp = dayjs().unix()
+
+          const formattedPoolReserves = formatReserves({
+            reserves: reservesArray,
+            currentTimestamp,
+            marketReferenceCurrencyDecimals:
+              baseCurrencyData.marketReferenceCurrencyDecimals,
+            marketReferencePriceInUsd:
+              baseCurrencyData.marketReferenceCurrencyPriceInUsd,
+          })
+
+          const userSummary = formatUserSummaryAndIncentives({
+            currentTimestamp,
+            marketReferencePriceInUsd:
+              baseCurrencyData.marketReferenceCurrencyPriceInUsd,
+            marketReferenceCurrencyDecimals:
+              baseCurrencyData.marketReferenceCurrencyDecimals,
+            userReserves: userReservesArray,
+            formattedReserves: formattedPoolReserves,
+            userEmodeCategoryId: userReserves.userEmodeCategoryId,
+            reserveIncentives,
+            userIncentives,
+          });
+          setUserDetails(userSummary)
+        } catch (error) {
+          console.error("Failed to fetch contract data:", error)
+          // Handle errors, e.g., show an error message to the user
+        }
+      })()
+    }
+  }, [particleProvider, account])
+
+  return (
+    <>
+      <div className="flex">
+        <SideNavbarNoSSR />
+        <div className="flex flex-col gap-5  w-full p-4 my-16">
+          <PageTitle title="AAVE Management" />
+          {/* @ts-ignore */}
+          {address && (
+            <CardContent>
+              <div>SmartAccount Address</div>
+              {address}
+              <div>{balance}</div>
+              </CardContent>
+          )}
+          <section className="grid grid-cols-1  gap-4 transition-all lg:grid-cols-2">
+          {
+              smartAccount ? (
+                <>
+                  <SupplyCard
+                    aaprovider={customProvider}
+                    smartAccount={smartAccount}
+                  />
+                  <BorrowCard
+                    aaprovider={customProvider}
+                    smartAccount={smartAccount}
+                  />
+                  <RepayCard
+                    aaprovider={customProvider}
+                    smartAccount={smartAccount}
+                  />
+                </>
+              ) : (
+                <div>Loading</div>
+              )
+            }
+          </section>
+        </div>
+      </div>
+    </>
+  )
+}
